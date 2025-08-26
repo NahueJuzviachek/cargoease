ee14971e20b3f0ec86dc80d249ac809b7267acc9
lxogod <leonelgiraudo05@gmail.com>
2025-08-26 03:55:45 -0300

    Agrego cambios en el proyecto Django

--- home/views.py
+++ home/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required
+#@login_required
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home/home.html")
+
+def clientes(request):
+    return render(request, "clientes.html")
+    
+def conductores(request):
+    return render(request, "conductores.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def vehiculos(request):
+    return render(request, "vehiculos.html")
+
+def reportes(request):
+    return render(request, "reportes.html")
+
