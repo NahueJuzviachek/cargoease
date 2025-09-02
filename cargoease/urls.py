0152c90a7f1fd093c099a69a1102b576e12fc9f6
lxogod <leonelgiraudo05@gmail.com>
2025-09-02 10:26:02 -0300

    conductores

--- cargoease/urls.py
+++ cargoease/urls.py
@@ -3,8 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 👇 usa tus rutas de login/logout
-    path('', include('login.urls')),  # asumiendo que tu app se llama "login"
+    path('', include('login.urls')),
     path('', include('home.urls')),
     path('clientes/', include('clientes.urls')),
     path('conductores/', include('conductores.urls')),
